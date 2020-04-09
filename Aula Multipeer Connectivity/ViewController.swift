5e683a3b96dee344837be4caf1c7caf4e3de6904
Julio Brazil <julio_bbl@hotmail.com>
2020-04-09 15:31:59 -0300

    code

--- Aula Multipeer Connectivity/ViewController.swift
+++ Aula Multipeer Connectivity/ViewController.swift
@@ -1,20 +1,85 @@
-//
-//  ViewController.swift
-//  Aula Multipeer Connectivity
-//
-//  Created by Julio Brazil Bellucci Lopes on 08/04/20.
-//  Copyright © 2020 Julio Brazil Bellucci Lopes. All rights reserved.
-//
-
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCNearbyServiceAdvertiserDelegate {
+    @IBOutlet weak var chatView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var serviceType = "chat-teste"
+    
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var advertiser: MCNearbyServiceAdvertiser!
+    var browser: MCNearbyServiceBrowser!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //TODO: criar um MCPeerID
+        
+        //TODO: criar uma MCSession
+        
+        //TODO: criar um advertiser para começar a transmitir
+        
+        //TODO: criar um browser para começar a procurar peers
     }
-
-
+    
+    @IBAction func didPressSend(_ sender: Any) {
+        self.send(text: textField.text ?? "")
+    }
+    
+    func printMessage(_ message: String, from sender: String? = nil) {
+        var text = "\n"
+        if let sender = sender {
+            text += "\(sender): "
+        }
+        text += message
+        DispatchQueue.main.async { [unowned self] in
+            self.chatView.text += text
+        }
+    }
+    
+    func send(text: String) {
+        //TODO: enviar mensagem para os peers conectados
+    }
+    
+    
+    //MARK: - MCNearbyServiceBrowserDelegate
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        //TODO: enviar um convite quando achar um peer
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        //do something when the connection with a peer is lost
+    }
+    
+    
+    //MARK: - MCNearbyServiceAdvertiserDelegate
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        //TODO: aceitar o convite quando receber um
+    }
+    
+    //MARK: - MCSessionDelegate
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        //TODO: printar qual o status
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        //TODO: tratar o recebimento da mensagem
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        //do something when a data stream is received
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        //do something when resource starts being received
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        //do something when resource has been received
+    }
 }
-
